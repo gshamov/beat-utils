016a592a16ff3d9813d4c25b1a1e8ea1a69e7316
Grigory <gas5x@yahoo.com>
2019-01-17 18:06:41 -0600

    Update set.go
    Added the dropKV function

--- set.go
+++ set.go
@@ -10,3 +10,19 @@ func List2Set (ss []string) ( mm map[string]struct{}) {
     }
     return mm
 }
+
+func DropKVs(list []string, mm map[string]interface{} ) {
+    // drops key-value pairs from map mm if key is not present in the list 
+    // the use of this function is to prune unwanted metrics without much thinking
+        metrics := List2Set(list)
+        var keys []string
+    // making list of keys to iterate over (deletion while iterating looks dangerous)
+        for k := range (mm) {
+               keys = append (keys, k)
+        }
+        for _, k := range(keys) {
+                        if  _, found := metrics[k] ; ! found {
+                            delete(mm, k)
+                }
+         }
+}
